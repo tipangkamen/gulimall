760f76dc684eb6fe50a27eb52fceea2edf2cd5e2
zhuguancheng <1005071600@qq.com>
2021-08-13 10:34:38 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/main/java/com/xiaozhu/gulimall/gulimallcoupon/GulimallCouponApplication.java
@@ -0,0 +1,13 @@
+package com.xiaozhu.gulimall.gulimallcoupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallCouponApplication.class, args);
+    }
+
+}
---
+++ gulimall-coupon/src/test/java/com/xiaozhu/gulimall/gulimallcoupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaozhu.gulimall.gulimallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
